73d2b00f9c29672c1ef3f513b2987e581f2c6432
Enrique Algar Ceular <ealgar-c@student.42malaga.com>
2024-05-08 20:39:46 +0200

    feat: maria db finished and started wp

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -2,7 +2,7 @@
 
 echo "DROP DATABASE IF EXISTS ${DB_NAME};" > setup.sql
 echo "CREATE DATABASE ${DB_NAME};" >> setup.sql
-echo "CREATE USER '${DB_USER}'@$'%' IDENTIFIED BY '${DB_PASSWD}';" >> setup.sql
+echo "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWD}';" >> setup.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';" >> setup.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%';" >> setup.sql
 echo "FLUSH PRIVILEGES;" >> setup.sql
@@ -10,6 +10,6 @@ echo "FLUSH PRIVILEGES;" >> setup.sql
 chmod 777 setup.sql
 mv setup.sql /run/mysqld/setup.sql
 chown -R mysql:root /var/run/mysqld
-tail -f
-mariadb $DB_NAME < run/mysqld/setup.sql
+# service mariadb start
+mariadbd --init-file /run/mysqld/setup.sql
 # mysql -u $DB_USER -p $DB_PASSWD < /run/mysqld/setup.sql
--- srcs/requirements/nginx/tools/nginx_init.sh
+++ srcs/requirements/nginx/tools/nginx_init.sh
@@ -4,7 +4,7 @@
 mkdir /etc/nginx/ssl
 
 # create the ssl certificate and key
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out $CERT_PATH -keyout $CERTKEY_PATH -subj "/C=ES/ST=Andalucia/L=Malaga/O=42/OU=42/CN=$DOM_NAME/UID=$LOGIN"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out $CERT_PATH -keyout $CERTKEY_PATH -subj "/C=SP/ST=Andalucia/L=Malaga/O=42/OU=42/CN=$DOM_NAME/UID=$LOGIN"
 
 # replace hostname, crt and key paths in the nginx.conf file
 sed -i "s/dom_name/$DOM_NAME/" /etc/nginx/nginx.conf
